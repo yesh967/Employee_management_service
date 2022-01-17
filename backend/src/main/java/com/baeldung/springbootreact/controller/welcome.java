ed1fda019ce67c1816fc06b1c0145ece93366bfc
Yesh Pareek <yesh.pareek@intelliswift.com>
2022-01-17 14:32:26 +0530

    heroku tested

--- backend/src/main/java/com/baeldung/springbootreact/controller/welcome.java
+++ backend/src/main/java/com/baeldung/springbootreact/controller/welcome.java
@@ -23,7 +23,7 @@ public class welcome {
                 "<br> This is landing page for Employee Task Management Project\n" +
                 "<br> This application can be used to create new employees, their tasks add contact details of these same employees ,delete tasks if desired once completed update employee , their tasks and contact details too  \n" +
                 "<br> <a  style=\"color: aqua;\"  href=\"https://employee-task-management.herokuapp.com/swagger-ui.html\"> Endpoints for utilising Employee task management service </a>\n" +
-                "<br>\n" +
+                "<br> \n" +
                 "<br> \n" +
 
 
